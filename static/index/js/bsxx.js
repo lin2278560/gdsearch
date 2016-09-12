adae39de4202c909df8b7c94cd7b813717044f3f
lxrmido <lxrmido@rMBP.local>
2016-09-12 17:42:11 +0800

    no message

--- static/index/js/bsxx.js
+++ static/index/js/bsxx.js
@@ -50,12 +50,63 @@ $(function(){
 			return;
 		}
 		G.call('search.bsxx', m, function(c, d){
+			$doc = $('<div>' + d.result + '</div>');
+			function qm(pt, wd){
+			    var r = pt.exec(wd);
+			    if(r && r.length && r.length > 1){
+			        return r[1];
+			    }else{
+			        return undefined;
+			    }
+			}
+			function qs($ct){
+		        var rsAmount = qm(/共找到([\d]+)条数据/, $ct.html()) || 0;
+		        var rsPages = [];
+		        $ct.find('#pagelist a').each(function(){
+		            if(this.className == 'curpage'){
+		                rsPages.push({
+		                    url  : '#',
+		                    text : $(this).html()
+		                });
+		            }else{
+		                rsPages.push({
+		                    url  : 'javascript:' + (($(this).attr('onclick'))),
+		                    text : $(this).html()
+		                });
+		            }
+		        });
+		        var rsNavs = [];
+		        $ct.find('.itempanel').each(function(){
+		            var $a = $(this).find('a');
+		            var $contentDiv = $(this).find('.content');
+		            var contentsMap = $contentDiv.map(function(){
+		                    return $(this).html().replace(/\n+/g, "\n").replace(/\n/g, "<br>").replace(/<em>/g, '<font color="#C0392B">').replace(/<\/em>/g, '</font>');
+		            });
+		            var i;
+		            var content = '';
+		            for(i = 0; i < contentsMap.length; i ++){
+		            	content += contentsMap[i];
+		            }
+		            rsNavs.push({
+		                content : content,
+		                date : '',
+		                url : ($a.attr('href')),
+		                title : $a.html().replace(/<em>/g, '<font color="#C0392B">').replace(/<\/em>/g, '</font>')
+		            });
+		        });
+		        return {
+		            amount : rsAmount,
+		            pages : rsPages,
+		            navs : rsNavs
+		        };
+		    }
+		    var qsrs = qs($doc);
 			$('#page-list').refreshPage({
 				cur : G.request['page'] || 1,
-				max : d.page_total
+				max : Math.ceil(qsrs.amount / 10)
 			});
 			$('#list-body').refreshList({
-				list : d.list
+				list : qsrs.navs
 			});
 		}, function(c, m){
 			alert(m)
